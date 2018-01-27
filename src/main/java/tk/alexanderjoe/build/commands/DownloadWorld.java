e752adfbd143ea9335173d9594dbdb04953ed77a
Alexander <alexanderjoemc@gmail.com>
2018-01-27 01:19:52 -0500

    Working world downloader.

--- src/main/java/tk/alexanderjoe/build/commands/DownloadWorld.java
+++ src/main/java/tk/alexanderjoe/build/commands/DownloadWorld.java
@@ -3,12 +3,12 @@ package tk.alexanderjoe.build.commands;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
-import org.bukkit.Color;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.json.JSONObject;
+import tk.alexanderjoe.build.build;
 import tk.alexanderjoe.build.utils.ColorTranslator;
 import tk.alexanderjoe.build.utils.Zip;
 
@@ -22,36 +22,37 @@ public class DownloadWorld implements CommandExecutor {
         if(cmd.getName().equalsIgnoreCase("downloadworld")) {
             if(sender instanceof Player) {
                 Player p = (Player) sender;
-                String worldname = p.getWorld().getName().toString();
+                String world = p.getWorld().getName().toString();
                 File path = p.getWorld().getWorldFolder();
                 if(!path.exists()) {
-                    p.sendMessage(ColorTranslator.translate("&cError! World doesn't exist."));
+                    p.sendMessage(ColorTranslator.translate(build.prefix + "&cError! World doesn't exist."));
                     return true;
                 } else {
-                    p.sendMessage(ColorTranslator.translate("&eCompressing world..."));
+                    p.sendMessage(ColorTranslator.translate(build.prefix + "&eCompressing world..."));
                     String randy = UUID.randomUUID().toString().substring(0, 3);
-                    File zip = new File(worldname + "-" + randy + ".zip");
+                    File zip = new File(world + "-" + randy + ".zip");
 
                     try {
                         Zip.create(path, zip);
                     } catch(Exception e) {
                         e.printStackTrace();
-                        p.sendMessage(ColorTranslator.translate("&cFailed to compress " + worldname + "."));
+                        p.sendMessage(ColorTranslator.translate(build.prefix + "&cFailed to compress " + world + "."));
                         return true;
                     }
 
-                    p.sendMessage(ColorTranslator.translate("&eUploading world..."));
+                    p.sendMessage(ColorTranslator.translate(build.prefix + "&eUploading world..."));
                     try {
                         HttpResponse<JsonNode> json = Unirest.post("http://file.io").field("file", zip).asJson();
                         JSONObject object = json.getBody().getObject();
                         zip.delete();
+                        sender.sendMessage(ColorTranslator.translate(build.prefix + "&eUpload complete: " + object.getString("link")));
                     } catch(Exception e) {
                         e.printStackTrace();
-                        p.sendMessage(ColorTranslator.translate("&cFailed to upload world..."));
+                        p.sendMessage(ColorTranslator.translate(build.prefix + "&cFailed to upload world..."));
                     }
                 }
             } else {
-                sender.sendMessage(ColorTranslator.translate("&cYou must be a player to use this command!"));
+                sender.sendMessage(ColorTranslator.translate(build.prefix + "&cYou must be a player to use this command!"));
                 return true;
             }
         }
